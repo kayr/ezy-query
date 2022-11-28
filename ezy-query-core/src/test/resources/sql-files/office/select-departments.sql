d5d3c1f95028720d35501ae2ec7d250c6a120078
Ronald Kayondo <noryak1234@gmail.com>
2022-11-28 15:04:33 +0000

    Clean up

--- ezy-query-core/src/test/resources/sql-files/office/select-departments.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    c.customerName   AS customerName,
-    e.employeeNumber AS employeeRep,
-    o.addressLine1   AS employeeOffice,
-    o.country        AS employeeCounty
-FROM offices o
-    LEFT JOIN employees e ON o.officeCode = e.officeCode
-    LEFT JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber

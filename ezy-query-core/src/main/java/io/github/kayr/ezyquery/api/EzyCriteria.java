6938d2297049da46a7df4ecb35b764b7c4337ec2
Ronald Kayondo <noryak1234@gmail.com>
2024-02-06 16:42:58 +0000

    clean up

--- ezy-query-core/src/main/java/io/github/kayr/ezyquery/api/EzyCriteria.java
+++ ezy-query-core/src/main/java/io/github/kayr/ezyquery/api/EzyCriteria.java
@@ -1,161 +1,178 @@
 package io.github.kayr.ezyquery.api;
 
-import static java.util.Collections.singletonList;
-
 import io.github.kayr.ezyquery.api.cnd.Cnd;
 import io.github.kayr.ezyquery.api.cnd.ICond;
 import io.github.kayr.ezyquery.util.Elf;
 import io.github.kayr.ezyquery.util.MapUtil;
-import java.util.*;
 import lombok.AccessLevel;
 import lombok.Builder;
+
+import java.util.*;
+
+import static java.util.Collections.singletonList;
 
 @lombok.Getter
 @lombok.Builder(toBuilder = true, access = AccessLevel.PRIVATE)
 public class EzyCriteria {
 
-  public static final String SORT_BY_MAP_PARAM = "_sortby";
-  public static final String OFFSET_PARAM = "_offset";
-  public static final String LIMIT_PARAM = "_limit";
+    public static final String SORT_BY_MAP_PARAM = "_sortby";
+    public static final String OFFSET_PARAM = "_offset";
+    public static final String LIMIT_PARAM = "_limit";
 
-  @Builder.Default private List<String> columns = new ArrayList<>();
-  @Builder.Default private List<ICond> conditions = new ArrayList<>();
-  @Builder.Default private List<NamedParamValue> paramValues = new ArrayList<>();
+    @Builder.Default
+    private List<String> columns = new ArrayList<>();
+    @Builder.Default
+    private List<ICond> conditions = new ArrayList<>();
+    @Builder.Default
+    private List<NamedParamValue> paramValues = new ArrayList<>();
 
-  @Builder.Default private List<Sort> sorts = new ArrayList<>();
+    @Builder.Default
+    private List<Sort> sorts = new ArrayList<>();
 
-  @Builder.Default private Long offset = 0L;
-  @Builder.Default private Integer limit = 50;
+    @Builder.Default
+    private Long offset = 0L;
+    @Builder.Default
+    private Integer limit = 50;
 
-  @Builder.Default private boolean count = false;
+    @Builder.Default
+    private boolean count = false;
 
-  // region Static methods
+    // region Static methods
 
-  /** Convenience method just to better communicate the intention */
-  public static EzyCriteria selectAll() {
-    return builder().build();
-  }
-
-  public static EzyCriteria select(String... columns) {
-    return builder().columns(Arrays.asList(columns)).build();
-  }
-
-  public static EzyCriteria selectCount() {
-    return builder().count(true).build();
-  }
-
-  public static EzyCriteria fromMap(Map<String, ?> criteria) {
-    HashMap<String, List<?>> map = new HashMap<>();
-    for (Map.Entry<String, ?> entry : criteria.entrySet()) {
-      map.put(entry.getKey(), singletonList(entry.getValue()));
+    /**
+     * Convenience method just to better communicate the intention
+     */
+    public static EzyCriteria selectAll() {
+        return builder().build();
     }
-    return fromMvMap(map);
-  }
 
-  public static EzyCriteria fromMvMap(Map<String, List<?>> criteria) {
-
-    List<Sort> sortByStr = extractSort(criteria);
-
-    Integer limit =
-        Optional.ofNullable(MapUtil.firstValue(criteria, LIMIT_PARAM))
-            .map(Object::toString)
-            .map(Integer::parseInt)
-            .orElse(50);
-
-    Long offset =
-        Optional.ofNullable(MapUtil.firstValue(criteria, OFFSET_PARAM))
-            .map(Object::toString)
-            .map(Long::parseLong)
-            .orElse(0L);
-
-    Map<String, List<?>> condOnlyMap =
-        Elf.remove(criteria, SORT_BY_MAP_PARAM, OFFSET_PARAM, LIMIT_PARAM);
-    List<ICond> conditions = singletonList(Cnd.fromMvMap(condOnlyMap));
-
-    return EzyCriteria.builder()
-        .conditions(conditions)
-        .limit(limit)
-        .offset(offset)
-        .sorts(sortByStr)
-        .build();
-  }
-
-  private static List<Sort> extractSort(Map<String, List<?>> criteria) {
-    Object firstValue = MapUtil.firstValue(criteria, SORT_BY_MAP_PARAM);
-    return Elf.safeMap(Elf.toString(firstValue), Sort::parse);
-  }
-
-  private static String first(Map<String, List<?>> criteria, String key) {
-    List<?> list = criteria.get(key);
-    if (Elf.isEmpty(list)) return null;
-    return list.get(0).toString();
-  }
-  // endregion
-
-  // region Builder methods
-  public EzyCriteria addSelect(String... columns) {
-    return toBuilder().columns(Elf.addAll(this.columns, columns)).build();
-  }
-
-  public EzyCriteria where(ICond... conds) {
-    return toBuilder().conditions(Elf.addAll(this.conditions, conds)).build();
-  }
-
-  public EzyCriteria offset(@lombok.NonNull Long offset) {
-    return toBuilder().offset(offset).build();
-  }
-
-  public EzyCriteria limit(@lombok.NonNull Integer limit) {
-    return toBuilder().limit(limit).build();
-  }
-
-  public EzyCriteria limit(@lombok.NonNull Integer limit, @lombok.NonNull Long offset) {
-    return toBuilder().limit(limit).offset(offset).build();
-  }
-
-  public EzyCriteria count() {
-    return toBuilder().count(true).build();
-  }
-
-  public EzyCriteria orderBy(Sort... sort) {
-    return toBuilder().sorts(Elf.addAll(this.sorts, sort)).build();
-  }
-
-  public EzyCriteria orderBy(String... sort) {
-    List<Sort> sortList = new ArrayList<>();
-    for (String s : sort) {
-      List<Sort> parse = Sort.parse(s);
-      sortList.addAll(parse);
+    public static EzyCriteria select(String... columns) {
+        return builder().columns(Arrays.asList(columns)).build();
     }
-    return toBuilder().sorts(Elf.combine(this.sorts, sortList)).build();
-  }
 
-  public EzyCriteria setParam(NamedParam namedParam, Object value) {
-    return toBuilder()
-        .paramValues(Elf.addAll(this.paramValues, new NamedParamValue(namedParam, value)))
-        .build();
-  }
+    public static EzyCriteria selectCount() {
+        return builder().count(true).build();
+    }
 
-  public EzyCriteria setCriteria(CriteriaHolder criteria, ICond cond) {
-    return setParam(criteria.getName(), cond);
-  }
+    public static EzyCriteria fromMap(Map<String, ?> criteria) {
+        HashMap<String, List<?>> map = new HashMap<>();
+        for (Map.Entry<String, ?> entry : criteria.entrySet()) {
+            map.put(entry.getKey(), singletonList(entry.getValue()));
+        }
+        return fromMvMap(map);
+    }
 
-  // endregion
+    public static EzyCriteria fromMvMap(Map<String, List<?>> criteria) {
 
-  // region Read only
+        List<Sort> sortByStr = extractSort(criteria);
+        Integer limit = extractLimit(criteria);
+        Long offset = extractOffset(criteria);
+        List<ICond> conditions = toConds(criteria);
 
-  public List<String> getColumns() {
-    return Collections.unmodifiableList(columns);
-  }
+        return EzyCriteria.builder()
+                 .conditions(conditions)
+                 .limit(limit)
+                 .offset(offset)
+                 .sorts(sortByStr)
+                 .build();
+    }
 
-  public List<ICond> getConditions() {
-    return Collections.unmodifiableList(conditions);
-  }
+    private static List<ICond> toConds(Map<String, List<?>> criteria) {
+        Map<String, List<?>> condOnlyMap =
+          Elf.remove(criteria, SORT_BY_MAP_PARAM, OFFSET_PARAM, LIMIT_PARAM);
+        return singletonList(Cnd.fromMvMap(condOnlyMap));
+    }
 
-  public List<Sort> getSorts() {
-    return Collections.unmodifiableList(sorts);
-  }
+    private static Integer extractLimit(Map<String, List<?>> criteria) {
+        return Optional.ofNullable(MapUtil.firstValue(criteria, LIMIT_PARAM))
+                 .map(Object::toString)
+                 .map(Integer::parseInt)
+                 .orElse(50);
+    }
 
-  // endregion
+    private static Long extractOffset(Map<String, List<?>> criteria) {
+        return Optional.ofNullable(MapUtil.firstValue(criteria, OFFSET_PARAM))
+                 .map(Object::toString)
+                 .map(Long::parseLong)
+                 .orElse(0L);
+    }
+
+    private static List<Sort> extractSort(Map<String, List<?>> criteria) {
+        Object firstValue = MapUtil.firstValue(criteria, SORT_BY_MAP_PARAM);
+        return Elf.safeMap(Elf.toString(firstValue), Sort::parse);
+    }
+
+    private static String first(Map<String, List<?>> criteria, String key) {
+        List<?> list = criteria.get(key);
+        if (Elf.isEmpty(list)) return null;
+        return list.get(0).toString();
+    }
+    // endregion
+
+    // region Builder methods
+    public EzyCriteria addSelect(String... columns) {
+        return toBuilder().columns(Elf.addAll(this.columns, columns)).build();
+    }
+
+    public EzyCriteria where(ICond... conds) {
+        return toBuilder().conditions(Elf.addAll(this.conditions, conds)).build();
+    }
+
+    public EzyCriteria offset(@lombok.NonNull Long offset) {
+        return toBuilder().offset(offset).build();
+    }
+
+    public EzyCriteria limit(@lombok.NonNull Integer limit) {
+        return toBuilder().limit(limit).build();
+    }
+
+    public EzyCriteria limit(@lombok.NonNull Integer limit, @lombok.NonNull Long offset) {
+        return toBuilder().limit(limit).offset(offset).build();
+    }
+
+    public EzyCriteria count() {
+        return toBuilder().count(true).build();
+    }
+
+    public EzyCriteria orderBy(Sort... sort) {
+        return toBuilder().sorts(Elf.addAll(this.sorts, sort)).build();
+    }
+
+    public EzyCriteria orderBy(String... sort) {
+        List<Sort> sortList = new ArrayList<>();
+        for (String s : sort) {
+            List<Sort> parse = Sort.parse(s);
+            sortList.addAll(parse);
+        }
+        return toBuilder().sorts(Elf.combine(this.sorts, sortList)).build();
+    }
+
+    public EzyCriteria setParam(NamedParam namedParam, Object value) {
+        return toBuilder()
+                 .paramValues(Elf.addAll(this.paramValues, new NamedParamValue(namedParam, value)))
+                 .build();
+    }
+
+    public EzyCriteria setCriteria(CriteriaHolder criteria, ICond cond) {
+        return setParam(criteria.getName(), cond);
+    }
+
+    // endregion
+
+    // region Read only
+
+    public List<String> getColumns() {
+        return Collections.unmodifiableList(columns);
+    }
+
+    public List<ICond> getConditions() {
+        return Collections.unmodifiableList(conditions);
+    }
+
+    public List<Sort> getSorts() {
+        return Collections.unmodifiableList(sorts);
+    }
+
+    // endregion
 
 }
